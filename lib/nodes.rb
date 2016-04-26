b105e2c076f15c75f4cfdde334b6a6c90fc39677
Tommy Vagbratt <tommy@vagbratt.se>
2016-04-26 09:59:06 +0200

    random work on semantic analysis

--- lib/nodes.rb
+++ lib/nodes.rb
@@ -1,8 +1,31 @@
-class Program                   < Struct.new(:nodes); end
+require_relative "semantic/error.rb"
 
-class VarDeclaration            < Struct.new(:type, :name); end
-class ArrayDeclaration          < Struct.new(:type, :name, :num_elements); end
-class ExternFunctionDeclaration < Struct.new(:type, :name, :formals); end
+class Program                   < Struct.new(:nodes)
+    def check_semantics env
+        nodes.each do |node|
+            env.add node if node.delaration?
+        end
+        nodes.each do |node|
+
+        end
+    end
+end
+
+class VarDeclaration            < Struct.new(:type, :name)
+    def check_semantics env
+        env.add name, type
+    end
+end
+class ArrayDeclaration          < Struct.new(:type, :name, :num_elements)
+    def check_semantics env
+        env.add name, "#{type}_ARRAY"
+    end
+end
+class ExternFunctionDeclaration < Struct.new(:type, :name, :formals)
+    def check_semantics env
+        env.add name, type
+    end
+end
 
 class FunctionDeclaration       < Struct.new(:type, :name, :formals, :body); end
 class FunctionBody              < Struct.new(:declarations, :statments); end
@@ -34,7 +57,11 @@ class UnaryMinus                < Struct.new(:expr)
 end
 class Not                       < Struct.new(:expr); end
 
-class BinaryOperator            < Struct.new(:left, :right); end
+class BinaryOperator            < Struct.new(:left, :right)
+    def get_type env
+        return :INT if left.get_type(env) == :INT && right.get_type(env) == :INT
+    end
+end
 class AddNode                   < BinaryOperator; end
 class SubNode                   < BinaryOperator; end
 class MulNode                   < BinaryOperator; end
@@ -52,3 +79,4 @@ class FunctionCall              < Struct.new(:name, :args); end
 class Return                    < Struct.new(:expr); end
 class While                     < Struct.new(:condition, :block); end
 class If                        < Struct.new(:condition, :then_block, :else_block); end
+
