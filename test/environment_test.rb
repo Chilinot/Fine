0cfddb8497a0588d59dc05b914f9b5d427520e80
Tommy Vagbratt <tommy@vagbratt.se>
2016-04-27 09:11:29 +0200

    update test for environment.rb

--- test/environment_test.rb
+++ test/environment_test.rb
@@ -4,44 +4,44 @@ describe "environment" do
     context "it is empty" do
         let(:env) {Environment.new}
         it "foo is not defined" do
-            expect {env.lookup "foo"}.to raise_error(SemanticError)
+            expect {env["foo"]}.to raise_error(SemanticError)
         end
         it "foo is defined if we define foo" do
-            env.add "foo", :INT
-            expect(env.lookup "foo").to eq :INT
+            env["foo"] = {:type => :INT}
+            expect(env["foo"]).to eq :INT
         end
     end
     context "foo and bar is defined" do
         let(:env) do
             e = Environment.new
-            e.add "foo", :INT
-            e.add "bar", :CHAR
+            e.add "foo", {:type => :INT}
+            e.add "bar", {:type => :CHAR}
             e
         end
         it "foo and bar is defined" do
             expect(env["foo"]).to eq :INT
-            expect(env.lookup "bar").to eq :CHAR
+            expect(env["bar"]).to eq :CHAR
         end
         it "foobar is not defined" do
-            expect {env.lookup "foobar"}.to raise_error(SemanticError)
+            expect {env["foobar"]}.to raise_error(SemanticError)
         end
         it "foo can not be redefined in same scope" do
-            expect { env["foo"] = :BOOL }.to raise_error(SemanticError)
+            expect { env["foo"] = {:type => :BOOL} }.to raise_error(SemanticError)
         end
         context "push a new scope" do
             before(:each) { env.push_scope }
             it "foo and bar is still defined after we push a new scope" do
                 env.push_scope
-                expect(env.lookup "foo").to eq :INT
-                expect(env.lookup "bar").to eq :CHAR
+                expect(env["foo"]).to eq :INT
+                expect(env["bar"]).to eq :CHAR
             end
             it "foo can be added to the new scope" do
-                env["foo"] =  :BOOL
+                env["foo"] =  {:type => :BOOL}
                 expect(env["foo"]).to eq :BOOL
             end
             it "foo can not be added if we pop the new scope" do
                 env.pop_scope
-                expect {env.add "foo", :BOOL}.to raise_error(SemanticError)
+                expect { env["foo"] = {:type => :BOOL}}.to raise_error(SemanticError)
             end
         end
     end
