de2e75378f0c94293361cc880fd284fec9a9e292
Tommy Vagbratt <tommy@vagbratt.se>
2016-04-29 10:33:46 +0200

    fix environment tests

--- test/environment_test.rb
+++ test/environment_test.rb
@@ -7,20 +7,20 @@ describe "environment" do
             expect {env["foo"]}.to raise_error(SemanticError)
         end
         it "foo is defined if we define foo" do
-            env["foo"] = {:class => :INT}
-            expect(env["foo"]).to eq :INT
+            env["foo"] = {:class => :VARIABLE, :type => :INT}
+            expect(env["foo"][:type]).to eq :INT
         end
     end
     context "foo and bar is defined" do
         let(:env) do
             e = Environment.new
-            e.add "foo", {:class => :INT}
-            e.add "bar", {:class => :CHAR}
+            e.add "foo", {:class => :VARIABLE, :type => :INT}
+            e.add "bar", {:class => :VARIABLE, :type => :CHAR}
             e
         end
         it "foo and bar is defined" do
-            expect(env["foo"]).to eq :INT
-            expect(env["bar"]).to eq :CHAR
+            expect(env["foo"][:type]).to eq :INT
+            expect(env["bar"][:type]).to eq :CHAR
         end
         it "foobar is not defined" do
             expect {env["foobar"]}.to raise_error(SemanticError)
@@ -32,16 +32,16 @@ describe "environment" do
             before(:each) { env.push_scope }
             it "foo and bar is still defined after we push a new scope" do
                 env.push_scope
-                expect(env["foo"]).to eq :INT
-                expect(env["bar"]).to eq :CHAR
+                expect(env["foo"][:type]).to eq :INT
+                expect(env["bar"][:type]).to eq :CHAR
             end
             it "foo can be added to the new scope" do
-                env["foo"] =  {:class => :BOOL}
-                expect(env["foo"]).to eq :BOOL
+                env["foo"] =  {:class => :VARIABLE, :type => :BOOL}
+                expect(env["foo"][:type]).to eq :BOOL
             end
             it "foo can not be added if we pop the new scope" do
                 env.pop_scope
-                expect { env["foo"] = {:class => :BOOL}}.to raise_error(SemanticError)
+                expect { env["foo"] = {:class => :VARIABLE, :type => :BOOL}}.to raise_error(SemanticError)
             end
         end
     end
