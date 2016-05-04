7e34288e6cdad1ccbb4ecf710969de47f49c6e29
Tommy Vagbratt <tommy@vagbratt.se>
2016-05-04 14:29:48 +0200

    fix test programs

--- test/data/quiet/semantic/s01.c
+++ test/data/quiet/semantic/s01.c
@@ -1,5 +1,5 @@
-/* Some strange but legal expressions and statements. 
-   For more examples of semantically correct code, see noisy/simple 
+/* Some strange but legal expressions and statements.
+   For more examples of semantically correct code, see noisy/simple
 */
 
 int x;
@@ -10,22 +10,23 @@ int main(void) {
   char w;
 
   x = x+y+z+w;
-  
+
   x = z = 42;
 
   x == z == 42;
 
   x == (z = 99); // Not legal without the paren
 
-  while (x) x = 0;
+  while (x) { x = 0; }
 
-  if (123) y = 4; else y = 7;
+  if (123) { y = 4; } else { y = 7; }
 
   w = x > y;
 
   y = 0 < x < 10;
 
   42;
+  return 42;
 }
 
 
