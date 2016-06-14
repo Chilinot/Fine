65ac4c0732f83e4092f11850eceb6443df897814
Tommy Vagbratt <tommy@vagbratt.se>
2016-06-15 01:03:17 +0200

    allocate and map formals

--- lib/ir/ir.rb
+++ lib/ir/ir.rb
@@ -1,14 +1,14 @@
 
 class Temporary < Struct.new(:index)
     def fix_globals locals; end
-    def generate_llvm
+    def generate_llvm formal_map = nil
         "%#{index}"
     end
 end
 
 class Label < Struct.new(:name, :index)
     def fix_globals locals; end
-    def generate_llvm
+    def generate_llvm formal_map = nil
         "#{name}#{index}:"
     end
 end
@@ -30,18 +30,20 @@ class Id < Struct.new(:name, :global)
     def fix_globals locals
         self.global = !locals.include?(name)
     end
-    def generate_llvm
+    def generate_llvm formal_map = nil
         if global
-            "@#{name}"
+            return "@#{name}"
+        elsif formal_map and formal_map.include? name
+            return formal_map[name]
         else
-            "%#{name}"
+            return "%#{name}"
         end
     end
 end
 
 class Constant < Struct.new(:type, :value)
     def fix_globals locals; end
-    def generate_llvm
+    def generate_llvm formal_map = nil
         "#{value}"
     end
 end
@@ -63,7 +65,7 @@ def generate_ir ast
 end
 
 class Ir < Struct.new(:definitions)
-    def generate_llvm
+    def generate_llvm formal_map = nil
         code = ""
         definitions.each do |d|
             code << d.generate_llvm
@@ -77,8 +79,8 @@ class Eval < Struct.new(:destination, :expression);
         destination.fix_globals locals
         expression.fix_globals locals
     end
-    def generate_llvm
-        "#{destination.generate_llvm} = #{expression.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "#{destination.generate_llvm} = #{expression.generate_llvm(formal_map)}"
     end
 end
 
@@ -98,26 +100,36 @@ class LocalArray < Struct.new(:name, :size); end
 class LocalIntArray < LocalArray; def generate_llvm; "local [#{size} x i32] #{name}\n" end end
 class LocalCharArray < LocalArray; def generate_llvm; "local [#{size} x i8] #{name}\n" end end
 
-class FormalArgument < Struct.new(:name, :type, :temporary); end
+class FormalArgument < Struct.new(:name, :type, :temporary)
+    def generate_llvm formal_map = nil
+        "    #{temporary.generate_llvm} = alloca #{type}\n" +
+        "    store #{type} %#{name}, #{type}* #{temporary.generate_llvm}\n"
+    end
+end
 
 class Function < Struct.new(:name, :type, :formals, :declarations, :instructions)
-    def generate_llvm
+    def generate_llvm formal_map = nil
+        formal_allocation = ""
+        formal_to_temporary = {}
         formal_list = ""
         formals.each do |f|
             formal_list += ", " unless formal_list.empty?
-            formal_list += "#{llvm_type(f.type)} %#{f.name}"
+            formal_list += "#{f.type} %#{f.name}"
+            formal_allocation += f.generate_llvm
+            formal_to_temporary[f.name] = f.temporary.generate_llvm
         end
-        header = "\ndefine #{llvm_type(type)} @#{name}(#{formal_list}) {\n"
+
+        header = "\ndefine #{type} @#{name}(#{formal_list}) {\n"
 
         declaration_list = ""
         declarations.each do |d|
             declaration_list += "  " + d.generate_llvm + "\n"
         end
 
-        instruction_list = ""
+        instruction_list = formal_allocation
         instructions.each do |i|
             extra_indent = "  " unless i.instance_of? Label
-            instruction_list += "  #{extra_indent}" + i.generate_llvm + "\n"
+            instruction_list += "  #{extra_indent}" + i.generate_llvm(formal_to_temporary) + "\n"
         end
 
         header + declaration_list + instruction_list + "}"
@@ -128,9 +140,9 @@ class Return < Struct.new(:type, :op)
     def fix_globals locals
         op.fix_globals locals unless op == :VOID
     end
-    def generate_llvm
+    def generate_llvm formal_map = nil
         unless op == :VOID
-            "ret #{type} #{op.generate_llvm}"
+            "ret #{type} #{op.generate_llvm(formal_map)}"
         else
             "ret #{type}"
         end
@@ -142,8 +154,8 @@ class Binop < Struct.new(:type, :op1, :op2)
         op1.fix_globals locals
         op2.fix_globals locals
     end
-    def generate_llvm
-        "#{self.class.to_s.downcase} #{op1.generate_llvm} #{op2.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "#{self.class.to_s.downcase} #{type} #{op1.generate_llvm(formal_map)}, #{op2.generate_llvm(formal_map)}"
     end
 end
 
@@ -165,8 +177,8 @@ class ArrayElement < Struct.new(:type, :name, :num_elements, :index)
     def fix_globals locals
         global = locals.include? name
     end
-    def generate_llvm
-        "#{name}[#{index.generate_llvm}]"
+    def generate_llvm formal_map = nil
+        "getelementptr inbounds [#{num_elements} x #{type}]* #{identifier}, i32 0, i32 #{index.generate_llvm(formal_map)}"
     end
 end
 
@@ -174,16 +186,16 @@ class Not < Struct.new(:op)
     def fix_globals locals
         op.fix_globals locals
     end
-    def generate_llvm
-        "not #{op.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "not #{op.generate_llvm(formal_map)}"
     end
 end
 class Cast < Struct.new(:op, :from, :to)
     def fix_globals locals
         op.fix_globals locals
     end
-    def generate_llvm
-        "cast #{to} #{op.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "cast #{to} #{op.generate_llvm(formal_map)}"
     end
 end
 
@@ -191,8 +203,8 @@ class Load < Struct.new(:type, :source);
     def fix_globals locals
         source.fix_globals locals
     end
-    def generate_llvm
-        "load #{type} #{source.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "load #{type}* #{source.generate_llvm(formal_map)}"
     end
 end
 
@@ -201,30 +213,40 @@ class Store < Struct.new(:type, :destination, :source)
         destination.fix_globals locals
         source.fix_globals locals
     end
-    def generate_llvm
-        "store #{type} #{destination.generate_llvm} #{source.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "store #{type} #{source.generate_llvm(formal_map)}, #{type}* #{destination.generate_llvm(formal_map)}"
     end
 end
 class Call < Struct.new(:name, :argument_list)
     def fix_globals locals;
         name.fix_globals locals
     end
-    def generate_llvm
-        "call #{name.generate_llvm} #{argument_list.map { |a| a.generate_llvm }.join(" ")}"
+    def generate_llvm formal_map = nil
+        "call #{name.generate_llvm} #{argument_list.map { |a| a.generate_llvm(formal_map) }.join(" ")}"
     end
 end
 
 class Jump < Struct.new(:label)
     def fix_globals locals; end
-    def generate_llvm
-        "jump #{label.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "br label #{label.identifier}"
     end
 end
+
 class Branch < Struct.new(:condition, :true_branch, :false_branch)
     def fix_globals locals
         condition.fix_globals locals
     end
-    def generate_llvm
-        "br #{condition.generate_llvm} #{true_branch.generate_llvm} #{false_branch.generate_llvm}"
+    def generate_llvm formal_map = nil
+        "br i1 #{condition.generate_llvm(formal_map)}, label #{true_branch.identifier}, label #{false_branch.identifier}"
+    end
+end
+
+class Compare < Struct.new(:type, :value)
+    def fix_globals locals
+        value.fix_globals locals
+    end
+    def generate_llvm formal_map = nil
+        "icmp ne #{type} #{value.generate_llvm(formal_map)}, 0"
     end
 end
