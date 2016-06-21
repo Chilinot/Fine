93a37ed2bf345eb26dc982494c64deb25422253b
Tommy Vagbratt <tommy@vagbratt.se>
2016-06-21 13:46:48 +0200

    add util function for writing string to file

--- lib/utils.rb
+++ lib/utils.rb
@@ -7,6 +7,12 @@ def read_file filename
     end
 end
 
+def write_file filename, content
+    File.open(filename, "w") do |file|
+        file.write(content)
+    end
+end
+
 def type_to_s type
     type.to_s.downcase.gsub("_","-")
 end
